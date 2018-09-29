a86cca6465e5bcfac396f4afb75398d01036d2bb
AnastasiaKarpenko <anastasia.s.karpenko@gmail.com>
2018-09-29 21:50:26 +0300

    Loading and Responding to Our Options

--- app/src/main/java/ws/tilda/anastasia/todoapp/MainActivity.java
+++ app/src/main/java/ws/tilda/anastasia/todoapp/MainActivity.java
@@ -1,7 +1,9 @@
 package ws.tilda.anastasia.todoapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.actions, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.about) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
