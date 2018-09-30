6d442e65023f7cbcd9aa4966a48f5b3613198423
AnastasiaKarpenko <anastasia.s.karpenko@gmail.com>
2018-09-30 14:42:20 +0300

    Migrating to FragmentActivity

--- app/src/main/java/ws/tilda/anastasia/todoapp/AboutActivity.java
+++ app/src/main/java/ws/tilda/anastasia/todoapp/AboutActivity.java
@@ -1,10 +1,10 @@
 package ws.tilda.anastasia.todoapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 import android.webkit.WebView;
 
-public class AboutActivity extends AppCompatActivity {
+public class AboutActivity extends FragmentActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- app/src/main/java/ws/tilda/anastasia/todoapp/MainActivity.java
+++ app/src/main/java/ws/tilda/anastasia/todoapp/MainActivity.java
@@ -1,12 +1,12 @@
 package ws.tilda.anastasia.todoapp;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends FragmentActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
