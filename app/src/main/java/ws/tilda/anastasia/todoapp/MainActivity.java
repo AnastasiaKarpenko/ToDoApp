223b020b7c857f9b1cd9b12f84dff7422e41ec25
AnastasiaKarpenko <anastasia.s.karpenko@gmail.com>
2018-09-30 13:47:05 +0300

    Starting the activity

--- app/src/main/java/ws/tilda/anastasia/todoapp/MainActivity.java
+++ app/src/main/java/ws/tilda/anastasia/todoapp/MainActivity.java
@@ -1,7 +1,10 @@
 package ws.tilda.anastasia.todoapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.actions, menu);
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.about) {
+            startActivity(new Intent(this, AboutActivity.class));
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
