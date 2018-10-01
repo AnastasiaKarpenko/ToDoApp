0b5d31ac1edbfdbb771f11503fc9f92457be322d
AnastasiaKarpenko <anastasia.s.karpenko@gmail.com>
2018-10-01 16:05:55 +0300

    Renaming The Layout Resource

--- app/src/main/java/ws/tilda/anastasia/todoapp/RosterListFragment.java
+++ app/src/main/java/ws/tilda/anastasia/todoapp/RosterListFragment.java
@@ -14,6 +14,6 @@ public class RosterListFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.activity_main, container, false);
+        return inflater.inflate(R.layout.todo_roster, container, false);
     }
 }
