a13f1ce034f4ab96b18e3c316ad4d554e16936a4
AnastasiaKarpenko <anastasia.s.karpenko@gmail.com>
2018-10-01 17:16:15 +0300

    Defining a Builder

--- app/src/main/java/ws/tilda/anastasia/todoapp/ToDoModel.java
+++ app/src/main/java/ws/tilda/anastasia/todoapp/ToDoModel.java
@@ -1,4 +1,18 @@
 package ws.tilda.anastasia.todoapp;
 
+import com.google.auto.value.AutoValue;
+
+@AutoValue
 public abstract class ToDoModel {
+
+    static Builder builder () {
+        return new AutoValue_ToDoModel.Builder();
+    }
+
+    @AutoValue.Builder
+    public static abstract class Builder {
+        public abstract ToDoModel build();
+    }
+
+
 }
