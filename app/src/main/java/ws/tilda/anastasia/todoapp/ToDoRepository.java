bd881f02114573957a90099281ec990093122d3d
AnastasiaKarpenko <anastasia.s.karpenko@gmail.com>
2018-10-01 19:10:49 +0300

    Offering a Singleton

--- app/src/main/java/ws/tilda/anastasia/todoapp/ToDoRepository.java
+++ app/src/main/java/ws/tilda/anastasia/todoapp/ToDoRepository.java
@@ -1,4 +1,10 @@
 package ws.tilda.anastasia.todoapp;
 
 public class ToDoRepository {
+
+    private static volatile ToDoRepository INSTANCE = new ToDoRepository();
+
+    public synchronized static ToDoRepository get() {
+        return INSTANCE;
+    }
 }
