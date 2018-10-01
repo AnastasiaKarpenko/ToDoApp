4854ea2fc6b64de159b7286cb6517bc2de4b3141
AnastasiaKarpenko <anastasia.s.karpenko@gmail.com>
2018-10-01 19:16:32 +0300

    Creating some fake data

--- app/src/main/java/ws/tilda/anastasia/todoapp/ToDoRepository.java
+++ app/src/main/java/ws/tilda/anastasia/todoapp/ToDoRepository.java
@@ -1,8 +1,28 @@
 package ws.tilda.anastasia.todoapp;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ToDoRepository {
 
     private static volatile ToDoRepository INSTANCE = new ToDoRepository();
+
+    private List<ToDoModel> items = new ArrayList<>();
+
+    private ToDoRepository() {
+        items.add(ToDoModel.builder()
+                .description("Buy a copy of _Exploring Android_")
+                .notes("See https://wares.commonsware.com")
+                .isCompleted(true)
+                .build());
+        items.add(ToDoModel.builder()
+                .description("Complete all of the tutorials")
+                .build());
+        items.add(ToDoModel.builder()
+                .description("Write an app for somebody in my community")
+                .notes("Talk to some people at non-profit organizations to see what they need!")
+                .build());
+    }
 
     public synchronized static ToDoRepository get() {
         return INSTANCE;
