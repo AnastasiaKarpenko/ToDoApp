a65a6a2e2bd55df1561208f1e3caab4f44b003a8
AnastasiaKarpenko <anastasia.s.karpenko@gmail.com>
2018-10-01 19:44:31 +0300

    Test the Repository

---
+++ app/src/androidTest/java/ws/tilda/anastasia/todoapp/RepoTests.java
@@ -0,0 +1,25 @@
+package ws.tilda.anastasia.todoapp;
+
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.assertEquals;
+
+
+@RunWith(AndroidJUnit4.class)
+public class RepoTests {
+    private ToDoRepository repo;
+
+    @Before
+    public void setUp() {
+        repo = ToDoRepository.get();
+    }
+
+    @After
+    public void getAll() {
+        assertEquals(3, repo.all().size());
+    }
+}
