0b8349f5ad358dad9ea813f366c6f768c112770a
AnastasiaKarpenko <anastasia.s.karpenko@gmail.com>
2018-10-01 17:38:41 +0300

    Adding Properties

--- app/src/main/java/ws/tilda/anastasia/todoapp/ToDoModel.java
+++ app/src/main/java/ws/tilda/anastasia/todoapp/ToDoModel.java
@@ -2,16 +2,41 @@ package ws.tilda.anastasia.todoapp;
 
 import com.google.auto.value.AutoValue;
 
+import java.util.Calendar;
+
+import javax.annotation.Nullable;
+
 @AutoValue
 public abstract class ToDoModel {
 
-    static Builder builder () {
+    public abstract String id();
+
+    public abstract boolean isCompleted();
+
+    public abstract String description();
+
+    @Nullable
+    public abstract String notes();
+
+    public abstract Calendar createdOn();
+
+    static Builder builder() {
         return new AutoValue_ToDoModel.Builder();
     }
 
     @AutoValue.Builder
     public static abstract class Builder {
         public abstract ToDoModel build();
+
+        abstract Builder id(String id);
+
+        public abstract Builder isCompleted(boolean isCompleted);
+
+        public abstract Builder description(String description);
+
+        public abstract Builder notes(String notes);
+
+        public abstract Builder createdOn(Calendar date);
     }
 
 
