a1ceb489c7231e25471b7795f4c4a5f69d668f89
AnastasiaKarpenko <anastasia.s.karpenko@gmail.com>
2018-10-02 19:52:54 +0300

    Instantiating the Fragment

--- app/src/main/java/ws/tilda/anastasia/todoapp/DisplayFragment.java
+++ app/src/main/java/ws/tilda/anastasia/todoapp/DisplayFragment.java
@@ -1,6 +1,19 @@
 package ws.tilda.anastasia.todoapp;
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
 
 public class DisplayFragment extends Fragment {
+    public static final String ARG_ID = "id";
+
+    public DisplayFragment newInstance(ToDoModel model) {
+        DisplayFragment result = new DisplayFragment();
+
+        if (model != null) {
+            Bundle args = new Bundle();
+            args.putString(ARG_ID, model.id());
+            result.setArguments(args);
+        }
+        return result;
+    }
 }
