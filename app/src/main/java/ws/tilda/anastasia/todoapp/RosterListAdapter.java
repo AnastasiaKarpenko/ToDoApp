00e0404cbb7bbeddaeb5ff9b6fc100fbe5bcaa95
AnastasiaKarpenko <anastasia.s.karpenko@gmail.com>
2018-10-02 10:02:08 +0300

    Creating a Stub Adapter

---
+++ app/src/main/java/ws/tilda/anastasia/todoapp/RosterListAdapter.java
@@ -0,0 +1,24 @@
+package ws.tilda.anastasia.todoapp;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
+
+
+public class RosterListAdapter extends RecyclerView.Adapter<RosterRowHolder> {
+    @NonNull
+    @Override
+    public RosterRowHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RosterRowHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
