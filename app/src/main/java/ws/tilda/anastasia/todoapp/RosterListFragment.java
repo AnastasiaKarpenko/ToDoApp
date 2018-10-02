e8b3e64a0a23ca0f212d324153012fb39b963ce8
AnastasiaKarpenko <anastasia.s.karpenko@gmail.com>
2018-10-02 11:04:16 +0300

    Wiring Up the RecyclerView

--- app/src/main/java/ws/tilda/anastasia/todoapp/RosterListFragment.java
+++ app/src/main/java/ws/tilda/anastasia/todoapp/RosterListFragment.java
@@ -4,6 +4,9 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,9 +14,27 @@ import android.view.ViewGroup;
 
 public class RosterListFragment extends Fragment {
 
+    private RecyclerView mRecyclerView;
+    private View mEmpty;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.todo_roster, container, false);
+        View result = inflater.inflate(R.layout.todo_roster, container, false);
+
+        mRecyclerView = result.findViewById(R.id.items);
+        mEmpty = result.findViewById(R.id.empty);
+
+        return result;
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+        DividerItemDecoration decoration = new DividerItemDecoration(getActivity(), LinearLayoutManager.VERTICAL);
+        mRecyclerView.addItemDecoration(decoration);
+        mRecyclerView.setAdapter(new RosterListAdapter(this));
+        mEmpty.setVisibility(View.GONE);
     }
 }
