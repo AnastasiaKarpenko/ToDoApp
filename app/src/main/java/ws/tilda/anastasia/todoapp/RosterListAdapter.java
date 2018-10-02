a363c66f33ed44067cd068426e298d298099ea78
AnastasiaKarpenko <anastasia.s.karpenko@gmail.com>
2018-10-02 10:04:49 +0300

    Retrieving Our Model Data

--- app/src/main/java/ws/tilda/anastasia/todoapp/RosterListAdapter.java
+++ app/src/main/java/ws/tilda/anastasia/todoapp/RosterListAdapter.java
@@ -4,8 +4,16 @@ import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.ViewGroup;
 
+import java.util.List;
+
 
 public class RosterListAdapter extends RecyclerView.Adapter<RosterRowHolder> {
+    final private List<ToDoModel> models;
+
+    RosterListAdapter(RosterListFragment host) {
+        models = ToDoRepository.get().all();
+    }
+
     @NonNull
     @Override
     public RosterRowHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
