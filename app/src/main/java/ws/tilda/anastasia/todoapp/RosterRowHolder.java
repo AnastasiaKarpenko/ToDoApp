b36b8c63984d7f48e7e419c5187aaf1d66634089
AnastasiaKarpenko <anastasia.s.karpenko@gmail.com>
2018-10-02 09:59:56 +0300

    Adding a Stub ViewHolder

---
+++ app/src/main/java/ws/tilda/anastasia/todoapp/RosterRowHolder.java
@@ -0,0 +1,11 @@
+package ws.tilda.anastasia.todoapp;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+
+public class RosterRowHolder extends RecyclerView.ViewHolder {
+    public RosterRowHolder(View itemView) {
+        super(itemView);
+    }
+}
