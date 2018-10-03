4299174b65a0168ea13faea39006bf275632ba62
AnastasiaKarpenko <anastasia.s.karpenko@gmail.com>
2018-10-03 17:56:30 +0300

    Instantiating the Fragment

--- app/src/main/java/ws/tilda/anastasia/todoapp/EditFragment.java
+++ app/src/main/java/ws/tilda/anastasia/todoapp/EditFragment.java
@@ -1,6 +1,25 @@
 package ws.tilda.anastasia.todoapp;
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
 
 public class EditFragment extends Fragment {
+    public static final String ARG_ID = "id";
+
+    static EditFragment newInstance(ToDoModel model) {
+        EditFragment result = new EditFragment();
+
+        if (model != null) {
+            Bundle args = new Bundle();
+
+            args.putString(ARG_ID, model.id());
+            result.setArguments(args);
+        }
+
+        return result;
+    }
+
+    public String getModelId() {
+        return getArguments().getString(ARG_ID);
+    }
 }
