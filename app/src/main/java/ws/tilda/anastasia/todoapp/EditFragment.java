a81e7baf6c8b0d97aa952050fc72a26722e00911
AnastasiaKarpenko <anastasia.s.karpenko@gmail.com>
2018-10-05 20:08:01 +0300

    Populating the Layout

--- app/src/main/java/ws/tilda/anastasia/todoapp/EditFragment.java
+++ app/src/main/java/ws/tilda/anastasia/todoapp/EditFragment.java
@@ -1,10 +1,18 @@
 package ws.tilda.anastasia.todoapp;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import ws.tilda.anastasia.todoapp.databinding.TodoEditBinding;
 
 public class EditFragment extends Fragment {
     public static final String ARG_ID = "id";
+    private TodoEditBinding mBinding;
 
     static EditFragment newInstance(ToDoModel model) {
         EditFragment result = new EditFragment();
@@ -21,5 +29,19 @@ public class EditFragment extends Fragment {
 
     public String getModelId() {
         return getArguments().getString(ARG_ID);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        mBinding = TodoEditBinding.inflate(getLayoutInflater(), container, false);
+
+        return mBinding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        mBinding.setModel(ToDoRepository.get().find(getModelId()));
     }
 }
