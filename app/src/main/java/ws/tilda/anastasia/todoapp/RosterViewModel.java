341febed3d3b9c0ef31d1c2c2dac1ecf1ab3f9e2
Anastasia Karpenko <morganaloca@gmail.com>
2018-10-08 16:05:40 +0300

    Creating a Stub ViewModel

---
+++ app/src/main/java/ws/tilda/anastasia/todoapp/RosterViewModel.java
@@ -0,0 +1,11 @@
+package ws.tilda.anastasia.todoapp;
+
+import android.app.Application;
+import android.arch.lifecycle.AndroidViewModel;
+import android.support.annotation.NonNull;
+
+public class RosterViewModel extends AndroidViewModel {
+    public RosterViewModel(@NonNull Application application) {
+        super(application);
+    }
+}
