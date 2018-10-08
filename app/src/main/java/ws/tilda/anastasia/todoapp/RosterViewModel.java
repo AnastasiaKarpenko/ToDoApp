edab3c3f8cde4d28c5ee4055883f92398e4e959e
Anastasia Karpenko <morganaloca@gmail.com>
2018-10-08 16:45:33 +0300

    Holding a MutableLiveData

--- app/src/main/java/ws/tilda/anastasia/todoapp/RosterViewModel.java
+++ app/src/main/java/ws/tilda/anastasia/todoapp/RosterViewModel.java
@@ -2,10 +2,20 @@ package ws.tilda.anastasia.todoapp;
 
 import android.app.Application;
 import android.arch.lifecycle.AndroidViewModel;
+import android.arch.lifecycle.LiveData;
+import android.arch.lifecycle.MutableLiveData;
 import android.support.annotation.NonNull;
 
 public class RosterViewModel extends AndroidViewModel {
+    private final MutableLiveData<ViewState> states = new MutableLiveData<>();
+
     public RosterViewModel(@NonNull Application application) {
         super(application);
     }
+
+    public LiveData<ViewState> stateStream() {
+        return states;
+    }
+
+
 }
