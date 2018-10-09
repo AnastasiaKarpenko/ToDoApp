b68339c59dce75ed182ac0c5e0d446fbd4e767be
Anastasia Karpenko <morganaloca@gmail.com>
2018-10-09 15:35:24 +0300

    Defining Specific Actions

--- app/src/main/java/ws/tilda/anastasia/todoapp/Action.java
+++ app/src/main/java/ws/tilda/anastasia/todoapp/Action.java
@@ -1,4 +1,31 @@
 package ws.tilda.anastasia.todoapp;
 
+import com.google.auto.value.AutoValue;
+
 public abstract class Action {
+
+    @AutoValue
+    public static abstract class Add extends Action {
+        public abstract ToDoModel model();
+
+    }
+
+    @AutoValue
+    public static abstract class Edit extends Action {
+        public abstract ToDoModel model();
+    }
+
+    @AutoValue
+    public static abstract class Delete extends Action {
+        public abstract ToDoModel model();
+    }
+
+    @AutoValue
+    public static abstract class Show extends  Action {
+        public abstract ToDoModel current();
+    }
+
+    public static class Load extends Action {
+
+    }
 }
