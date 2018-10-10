28cfb7840ff42641fcb51f9137ae830f8d72a31e
Anastasia Karpenko <morganaloca@gmail.com>
2018-10-10 14:24:32 +0300

    Defining Specific Results

--- app/src/main/java/ws/tilda/anastasia/todoapp/Result.java
+++ app/src/main/java/ws/tilda/anastasia/todoapp/Result.java
@@ -1,4 +1,37 @@
 package ws.tilda.anastasia.todoapp;
 
-public class Result {
+import com.google.auto.value.AutoValue;
+
+import java.util.List;
+
+public abstract class Result {
+
+    /* This sets up five Result subclasses that mirror the five Action subclasses.
+    The biggest difference — besides the past tense on the class names —
+    is that Loaded now has the results of the data load: a List of our model objects. */
+
+    @AutoValue
+    public static abstract class Added extends Result {
+        public abstract ToDoModel model();
+    }
+
+    @AutoValue
+    public static abstract class Modified extends Result {
+        public abstract ToDoModel model();
+    }
+
+    @AutoValue
+    public static abstract class Deleted extends Result {
+        public abstract ToDoModel model();
+    }
+
+    @AutoValue
+    public static abstract class Showed extends Result {
+        public abstract ToDoModel current();
+    }
+
+    @AutoValue
+    public static abstract class Loaded extends Result {
+        public abstract List<ToDoModel> models();
+    }
 }
