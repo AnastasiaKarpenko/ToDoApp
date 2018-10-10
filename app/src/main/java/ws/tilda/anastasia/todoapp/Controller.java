6f436661e3b0c2e8e8550108daee1498ace604f6
Anastasia Karpenko <morganaloca@gmail.com>
2018-10-10 12:36:52 +0300

    Handling Add, Edit, and Delete Actions

--- app/src/main/java/ws/tilda/anastasia/todoapp/Controller.java
+++ app/src/main/java/ws/tilda/anastasia/todoapp/Controller.java
@@ -8,13 +8,31 @@ public class Controller {
 
     //We need to process our actions
     private void processImpl(Action action) {
-
+        if (action instanceof Action.Add) {
+            add(((Action.Add) action).model());
+        } else if (action instanceof Action.Edit) {
+            modify(((Action.Edit) action).model());
+        } else if (action instanceof Action.Delete) {
+            delete(((Action.Delete) action).model());
+        }
     }
 
     public void subscribeToActions(Observable<Action> actionStream) {
         actionStream
                 .observeOn(Schedulers.io())
                 .subscribe(this::processImpl);
+    }
+
+    private void add(ToDoModel model) {
+        toDoRepo.add(model);
+    }
+
+    private void modify(ToDoModel model) {
+        toDoRepo.replace(model);
+    }
+
+    private void delete(ToDoModel model) {
+        toDoRepo.delete(model);
     }
 
 
