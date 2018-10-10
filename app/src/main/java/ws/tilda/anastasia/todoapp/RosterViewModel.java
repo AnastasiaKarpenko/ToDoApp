2a6076b969304bd80a3e321ad6089850791364f9
Anastasia Karpenko <morganaloca@gmail.com>
2018-10-10 12:46:43 +0300

    Connecting to the Controller

--- app/src/main/java/ws/tilda/anastasia/todoapp/RosterViewModel.java
+++ app/src/main/java/ws/tilda/anastasia/todoapp/RosterViewModel.java
@@ -6,8 +6,11 @@ import android.arch.lifecycle.LiveData;
 import android.arch.lifecycle.MutableLiveData;
 import android.support.annotation.NonNull;
 
+import io.reactivex.subjects.PublishSubject;
+
 public class RosterViewModel extends AndroidViewModel {
     private final MutableLiveData<ViewState> states = new MutableLiveData<>();
+    private final PublishSubject<Action> actionSubject = PublishSubject.create();
 
     public RosterViewModel(@NonNull Application application) {
         super(application);
@@ -17,10 +20,18 @@ public class RosterViewModel extends AndroidViewModel {
                 .build();
 
         states.postValue(initial);
+
+        Controller controller = new Controller();
+        controller.subscribeToActions(actionSubject);
     }
+
 
     public LiveData<ViewState> stateStream() {
         return states;
+    }
+
+    public void process(Action action) {
+        actionSubject.onNext(action);
     }
 
 
