b619242aa59cfd2e0c0b8ae08949aeece7ece59d
Anastasia Karpenko <morganaloca@gmail.com>
2018-10-10 12:09:50 +0300

    Subscribing to Actions

--- app/src/main/java/ws/tilda/anastasia/todoapp/Controller.java
+++ app/src/main/java/ws/tilda/anastasia/todoapp/Controller.java
@@ -1,5 +1,21 @@
 package ws.tilda.anastasia.todoapp;
 
+import io.reactivex.Observable;
+import io.reactivex.schedulers.Schedulers;
+
 public class Controller {
     private final ToDoRepository toDoRepo = ToDoRepository.get();
+
+    //We need to process our actions
+    private void processImpl(Action action) {
+
+    }
+
+    public void subscribeToActions(Observable<Action> actionStream) {
+        actionStream
+                .observeOn(Schedulers.io())
+                .subscribe(this::processImpl);
+    }
+
+
 }
