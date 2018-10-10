e73ea33073922c65ce3b7f911c62e7e507f93eb0
Anastasia Karpenko <morganaloca@gmail.com>
2018-10-10 14:31:07 +0300

    Building Factory Methods

--- app/src/main/java/ws/tilda/anastasia/todoapp/Result.java
+++ app/src/main/java/ws/tilda/anastasia/todoapp/Result.java
@@ -2,9 +2,33 @@ package ws.tilda.anastasia.todoapp;
 
 import com.google.auto.value.AutoValue;
 
+import java.util.Collections;
 import java.util.List;
 
 public abstract class Result {
+
+    //Factory methods corresponding to the result types
+
+    public static Result added(ToDoModel model) {
+        return new AutoValue_Result_Added(model);
+    }
+
+    public static Result modified(ToDoModel model) {
+        return new AutoValue_Result_Modified(model);
+    }
+
+    public static Result deleted(ToDoModel model) {
+        return new AutoValue_Result_Deleted(model);
+    }
+
+    public static Result showed(ToDoModel current) {
+        return new AutoValue_Result_Showed(current);
+    }
+
+    public static Result loaded(List<ToDoModel> models) {
+        return new AutoValue_Result_Loaded(Collections.unmodifiableList(models));
+    }
+
 
     /* This sets up five Result subclasses that mirror the five Action subclasses.
     The biggest difference — besides the past tense on the class names —
