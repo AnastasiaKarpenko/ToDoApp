9164be4eb132ae0ad12aff4a89f9df8b1a84bcb9
Anastasia Karpenko <morganaloca@gmail.com>
2018-10-11 11:56:18 +0300

    Comments to myself

--- app/src/main/java/ws/tilda/anastasia/todoapp/RosterViewModel.java
+++ app/src/main/java/ws/tilda/anastasia/todoapp/RosterViewModel.java
@@ -11,11 +11,11 @@ import io.reactivex.subjects.PublishSubject;
 import io.reactivex.subjects.ReplaySubject;
 
 public class RosterViewModel extends AndroidViewModel {
-    public static final int LATEST_ONE = 1;
-    private final PublishSubject<Action> actionSubject = PublishSubject.create();
-    private final ReplaySubject<ViewState> stateSubject = ReplaySubject.createWithSize(LATEST_ONE);
     private LiveData<ViewState> states;
     private ViewState lastState = ViewState.empty().build();
+    private static final int LATEST_ONE = 1;
+    private final PublishSubject<Action> actionSubject = PublishSubject.create();
+    private final ReplaySubject<ViewState> stateSubject = ReplaySubject.createWithSize(LATEST_ONE); //caching the most recent data
 
 
     public RosterViewModel(@NonNull Application application) {
@@ -25,11 +25,24 @@ public class RosterViewModel extends AndroidViewModel {
         Controller controller = new Controller();
         controller.subscribeToActions(actionSubject);
 
+        /*
+        Last ViewState is blended with new Result and gets a new ViewState via foldResultIntoState().
+        To overcome limitations of LiveData and ReactiveDataStreams we needed to make the stream
+        itself cache the most recently seen ViewState in stateSubject.
+        */
         controller.resultStream()
                 .subscribe(result -> {
                     lastState = foldResultIntoState(lastState, result);
-                    stateSubject.onNext(lastState);
+                    stateSubject.onNext(lastState); //
                 }, stateSubject::onError);
+        /*
+        LiveData states is initialized using LifeDataReactiveStreams that comes from the dependency. fromPublisher() method
+        creates a LiveData object that knows how to subscribe to RxJava type and pass any received objects to subscribers.
+        fromPublisher() methods receives object that implements Publisher interface that is a part of ReactiveStreams initiative.
+        stateSubject is converted to Publisher interface via toFlowable(), that requires to supply a BackPressure strategy
+        (meaning "what to do if the pipeline gets blocked") and LATEST strategy means to keep the latest items in the pipeline
+        and eliminate the older ones if the is a backlog.
+        */
         states = LiveDataReactiveStreams
                 .fromPublisher(stateSubject.toFlowable(BackpressureStrategy.LATEST));
     }
@@ -43,14 +56,12 @@ public class RosterViewModel extends AndroidViewModel {
         actionSubject.onNext(action);
     }
 
-    private ViewState foldResultIntoState(@NonNull ViewState state, @NonNull Result result)
-            throws Exception {
+    private ViewState foldResultIntoState(@NonNull ViewState state,
+                                          @NonNull Result result) throws Exception {
         if (result instanceof Result.Added) {
             return state.add(((Result.Added) result).model());
         } else {
             throw new IllegalStateException("Unexpected result type: " + result.toString());
         }
     }
-
-
 }
