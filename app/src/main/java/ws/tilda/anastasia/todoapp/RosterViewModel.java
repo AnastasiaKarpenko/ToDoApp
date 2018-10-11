e907f980e8ee47c4ac3df9bdf0a27a805612a771
Anastasia Karpenko <morganaloca@gmail.com>
2018-10-11 11:12:25 +0300

    Publishing the ViewStates

--- app/src/main/java/ws/tilda/anastasia/todoapp/RosterViewModel.java
+++ app/src/main/java/ws/tilda/anastasia/todoapp/RosterViewModel.java
@@ -3,26 +3,35 @@ package ws.tilda.anastasia.todoapp;
 import android.app.Application;
 import android.arch.lifecycle.AndroidViewModel;
 import android.arch.lifecycle.LiveData;
-import android.arch.lifecycle.MutableLiveData;
+import android.arch.lifecycle.LiveDataReactiveStreams;
 import android.support.annotation.NonNull;
 
+import io.reactivex.BackpressureStrategy;
 import io.reactivex.subjects.PublishSubject;
+import io.reactivex.subjects.ReplaySubject;
 
 public class RosterViewModel extends AndroidViewModel {
-    private final MutableLiveData<ViewState> states = new MutableLiveData<>();
+    public static final int LATEST_ONE = 1;
     private final PublishSubject<Action> actionSubject = PublishSubject.create();
+    private final ReplaySubject<ViewState> stateSubject = ReplaySubject.createWithSize(LATEST_ONE);
+    private LiveData<ViewState> states;
+    private ViewState lastState = ViewState.empty().build();
+
 
     public RosterViewModel(@NonNull Application application) {
         super(application);
 
-        ViewState initial = ViewState.builder()
-                .items(ToDoRepository.get().all())
-                .build();
-
-        states.postValue(initial);
 
         Controller controller = new Controller();
         controller.subscribeToActions(actionSubject);
+
+        controller.resultStream()
+                .subscribe(result -> {
+                    lastState = foldResultIntoState(lastState, result);
+                    stateSubject.onNext(lastState);
+                }, stateSubject::onError);
+        states = LiveDataReactiveStreams
+                .fromPublisher(stateSubject.toFlowable(BackpressureStrategy.LATEST));
     }
 
 
@@ -34,7 +43,7 @@ public class RosterViewModel extends AndroidViewModel {
         actionSubject.onNext(action);
     }
 
-    private ViewState forldResultIntoState(@NonNull ViewState state, @NonNull Result result)
+    private ViewState foldResultIntoState(@NonNull ViewState state, @NonNull Result result)
             throws Exception {
         if (result instanceof Result.Added) {
             return state.add(((Result.Added) result).model());
