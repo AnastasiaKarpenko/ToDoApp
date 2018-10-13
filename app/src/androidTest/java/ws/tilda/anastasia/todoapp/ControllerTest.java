edd5a5029360f39958fcbd780db829df6801a616
AnastasiaKarpenko <anastasia.s.karpenko@gmail.com>
2018-10-13 17:59:44 +0300

    Adding a ControllerTest Class

---
+++ app/src/androidTest/java/ws/tilda/anastasia/todoapp/ControllerTest.java
@@ -0,0 +1,15 @@
+package ws.tilda.anastasia.todoapp;
+
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(AndroidJUnit4.class)
+public class ControllerTest {
+
+    @Test
+    public void controller() {
+
+    }
+}
