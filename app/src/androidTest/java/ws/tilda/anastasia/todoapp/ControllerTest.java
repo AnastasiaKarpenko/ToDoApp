15ad64d260cb0c16ef95d285044c2aaa51447f84
AnastasiaKarpenko <anastasia.s.karpenko@gmail.com>
2018-10-13 18:22:23 +0300

    Setting Up a Controller

--- app/src/androidTest/java/ws/tilda/anastasia/todoapp/ControllerTest.java
+++ app/src/androidTest/java/ws/tilda/anastasia/todoapp/ControllerTest.java
@@ -5,11 +5,23 @@ import android.support.test.runner.AndroidJUnit4;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import java.util.concurrent.LinkedBlockingQueue;
+
+import io.reactivex.subjects.PublishSubject;
+
 @RunWith(AndroidJUnit4.class)
 public class ControllerTest {
 
+
+
     @Test
     public void controller() {
+        final Controller controller = new Controller();
+        final PublishSubject<Action> actionSubject = PublishSubject.create();
+        final LinkedBlockingQueue<Result> receivedResults = new LinkedBlockingQueue<>();
+
+        controller.subscribeToActions(actionSubject);
+        controller.resultStream().subscribe(receivedResults::offer);
 
     }
 }
